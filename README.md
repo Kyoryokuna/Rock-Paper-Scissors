e528ddde7bb15852cd40c59b40573cb399058bc2
Sphamandla Mhlongo <bhebhejunior16@gmail.com>
2025-07-31 21:05:12 +0200

    added UI

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Rock-Paper-Scissors
 This is a java-script practice project
+
+this was a fun project to make as it has taught me a lot about how javascript and html interact.
+i got to practice DOM manipulation along with learning about git merging and branching.
--- script.js
+++ script.js
@@ -1,64 +1,80 @@
+const startBtn = document.getElementById("playBtn")
+const rockBtn = document.getElementById("rock")
+const paperBtn = document.getElementById("paper")
+const scissorsBtn = document.querySelector("#scissors")
+const hScore = document.querySelector("#hScore")
+const cScore = document.querySelector("#cScore")
+const notification = document.querySelector(".notify")
+const cimage = document.querySelector("#cimage")
+const himage = document.querySelector("#himage")
+const descripc = document.querySelector(".descripc")
+const descriph = document.querySelector(".descriph")
 
-function getComputerChoice() {
-    let choice;
-    let num = parseFloat(getRandomNumber().toFixed(0))
-
-    if (num === 1) {
-        choice = "rock"
-    } else if (num === 2) {
-        choice = "paper"
-    } else {
-        choice = "scissors"
-    }
-
-    return choice
-}
-
-function getPlayerChoice() {
-    const input = document.getElementById("userInput")
-    let choice = input.value
-    return choice
-}
-
-function getRandomNumber() {
-    return Math.random() * 3
-}
-
-let computerScore = 0
+let active = false
 let humanScore = 0
+let computerScore = 0
 
-function play() {
-    let computerChoice = getComputerChoice();
-    let playerChoice = getPlayerChoice();
+const Options = ["rock","paper","scissors"]
 
-    if (computerChoice === "rock" && playerChoice === "scissors") {
-        console.log(computerChoice + ` beats ` + playerChoice)
-        console.log("you lose")
-        computerScore++;
-    } else if (computerChoice === playerChoice) {
-        console.log(computerChoice + ` & ` + playerChoice)
-        console.log("Tie")
-    } else if (computerChoice === "scissors" && playerChoice === "paper") {
-        console.log(computerChoice + ` beats ` + playerChoice)
-        console.log("you lose")
-        computerScore++;
-    } else if (computerChoice === "paper" && playerChoice === "rock") {
-        console.log(computerChoice + ` beats ` + playerChoice)
-        console.log("you lose")
-        computerScore++;
-    } else if (playerChoice === "rock" && computerChoice === "scissors") {
-        console.log(playerChoice+ ` beats ` + computerChoice)
-        console.log("you win")
-        humanScore++;
-    } else if (playerChoice === "scissors" && computerChoice === "paper") {
-        console.log(playerChoice + ` beats ` + computerChoice)
-        console.log("you win")
-        humanScore++;
-    } else if (playerChoice === "paper" && computerChoice === "rock") {
-        console.log(playerChoice + ` beats ` + computerChoice)
-        console.log("you win")
-        humanScore++;
+const start = function() {
+    if (active === false) {
+        startBtn.textContent = "Game in progress..."
+        active = true
+        notification.textContent = "VS"
+        return
     }
-
-    console.log("playerScore: " + humanScore + " / computerScore: " + computerScore )
+    console.log("Game already in progress")
 }
+
+const randomNum = function() {
+    return Math.floor((Math.random() * 3))
+}
+
+const play = function(humChoice) {
+    if (!active) {
+        notification.textContent = "please start the game first"    
+        return
+    }
+    let compChoice = Options[randomNum()]
+    if (humChoice === "rock" && compChoice === "scissors" || humChoice === "paper" && compChoice === "rock" || humChoice === "scissors" && compChoice === "paper") {
+        notification.textContent = `${humChoice} beats ${compChoice}`
+        humanScore++
+    }else if (compChoice === "rock" && humChoice === "scissors" || compChoice === "paper" && humChoice === "rock" || compChoice === "scissors" && humChoice === "paper") {
+        notification.textContent = `${humChoice} loses to ${compChoice}`
+        computerScore++
+    }else{
+        notification.textContent = "Tie"
+    }
+    himage.src = `./images/${humChoice}.jpg`
+    cimage.src = `./images/${compChoice}.jpg`
+
+    descripc.textContent = compChoice
+    descriph.textContent = humChoice
+
+    hScore.textContent = `Human score: ${humanScore}`
+    cScore.textContent = `Bot score: ${computerScore}`
+
+    if (humanScore === 5) {
+        end("human")
+    }else if (computerScore === 5) {
+        end("computer")
+    }
+}
+const end = function(winner) {
+    if (winner === "human") {
+        notification.textContent = `Congratulations you won (${humanScore} / ${computerScore})`
+    }else{
+        notification.textContent = `Game Over!`
+    }
+    startBtn.textContent = "Play!"
+    humanScore = 0
+    computerScore = 0
+    hScore.textContent = `Human score: ${humanScore}`
+    cScore.textContent = `Bot score: ${computerScore}`
+    active = false
+}
+
+startBtn.addEventListener("click",start)
+rockBtn.addEventListener("click", () => play("rock"))
+paperBtn.addEventListener("click", () => play("paper"))
+scissorsBtn.addEventListener("click", () => play("scissor"))
